62702a8ff95c1c3cde958494194d172164a78424
Alexey Melezhik <melezhik@gmail.com>
2024-07-14 08:24:47 +0000

    new project

--- projects/update/.sparrowdo/sparrowrun.sh
+++ projects/update/.sparrowdo/sparrowrun.sh
@@ -8,7 +8,7 @@ cd .sparrowdo/
 export SP6_REPO=https://sparrowhub.io/repo
 export SP6_PREFIX=.sparrowdo/update
 export SP6_TAGS='SCM_URL=https://github.com/melezhik/ci.sparrowhub.git SCM_SHA=HEAD
-,SCM_SHA=d0bfc028,SCM_URL=https://github.com/melezhik/ci.sparrowhub.git,SCM_BRANCH=HEAD,SPARKY_PROJECT=update,SPARKY_JOB_ID=lfyebmdzqsrtjkioanvu2685289,SPARKY_WORKER=localhost,SPARKY_TCP_PORT=4000,SPARKY_API_TOKEN=rakuandsparky1983'
+,,SPARKY_PROJECT=update,SPARKY_JOB_ID=uivrloqsxjwnhcadtmgy.2140565,SPARKY_WORKER=localhost,SPARKY_TCP_PORT=4000,SPARKY_API_TOKEN=rakuandsparky1983'
 export SP6_FORMAT_COLOR=1
 raku -MSparrow6::Task::Repository -e Sparrow6::Task::Repository::Api.new.index-update
 raku -MSparrow6::DSL sparrowfile
