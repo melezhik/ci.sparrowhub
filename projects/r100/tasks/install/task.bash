aded79dc860dc7c63f2a3e13b6f0838f4614d046
Alexey Melezhik <melezhik@gmail.com>
2025-10-21 12:56:03 +0300

    fix

--- projects/r100/tasks/install/task.bash
+++ projects/r100/tasks/install/task.bash
@@ -3,9 +3,9 @@ echo "install $module"
 rm -rf log.txt
 
 if zef install $module 1>log.txt 2>&1; then
-  echo "module $m installation succeed"
+  echo "installation succeed"
   update_state success 1
  else
-  echo "module $m installation failed"
+  echo "installation failed"
   update_state success 0
 fi
